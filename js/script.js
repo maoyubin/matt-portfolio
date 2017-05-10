36726fb7e68bdada0743d422d61699c8e8bf604f
maoyubin <maoyubinabc@163.com>
2017-05-11 05:59:02 +0800

    a fixed nav bar disppear when scrolls down and reappear upon scrolling up

--- js/script.js
+++ js/script.js
@@ -33,4 +33,50 @@ jQuery(document).ready(function() {
 	    }
 	  }
 	});
+
+	// Hide Header on on scroll down
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var header = $('.main-menu');
+	var navbarHeight = header.outerHeight();
+
+	$(window).scroll(function(event){
+		didScroll = true;
+	});
+
+	setInterval(function() {
+		if (didScroll) {
+		hasScrolled();
+		didScroll = false;
+		}
+	}, 250);
+
+	function hasScrolled() {
+		var st = $(this).scrollTop();
+
+
+		console.log('navbarHeight='+navbarHeight);
+		console.log('st='+st);
+		console.log('lastScrollTop='+lastScrollTop);
+		console.log('Math.abs(lastScrollTop - st)='+Math.abs(lastScrollTop - st));
+		// Make sure they scroll more than delta
+		if(Math.abs(lastScrollTop - st) <= delta)
+			return;
+
+		// If they scrolled down and are past the navbar, add class .nav-up.
+		// This is necessary so you never see what is "behind" the navbar.
+		if (st > lastScrollTop && st > navbarHeight){
+			// Scroll Down
+			header.removeClass('nav-down').addClass('nav-up');
+		} else {
+			// Scroll Up
+			if(st + $(window).height() < $(document).height()) {
+			    header.removeClass('nav-up').addClass('nav-down');
+			}
+		}
+
+		lastScrollTop = st;
+	}
+
 });
